e1d665083e5508766a4e2677e0542359a7ceb9ec
Станислав Боровик <bsa.aliery@gmail.com>
2022-06-14 23:53:10 +0300

    Day and Night Manager
    Skybox

---
+++ Assets/Scripts/DayNightManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightManager : MonoBehaviour
+{
+    [Range(0, 1)]
+    public float timeOfDay;
+    public float dayDuration = 30f;
+
+    public AnimationCurve SunCurve;
+    public AnimationCurve MoonCurve;
+    public AnimationCurve SkyboxCurve;
+
+    public Material DaySkybox;
+    public Material NightSkybox;
+
+    public Light Sun;
+    public Light Moon;
+
+    private float sunIntensity;
+    private float moonIntensity;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sunIntensity = Sun.intensity;
+        moonIntensity = Moon.intensity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeOfDay += Time.deltaTime / dayDuration;
+        if (timeOfDay >= 1) timeOfDay -= 1;
+
+        RenderSettings.skybox.Lerp(NightSkybox, DaySkybox, SkyboxCurve.Evaluate(timeOfDay));
+        DynamicGI.UpdateEnvironment();
+        
+        Sun.transform.localRotation = Quaternion.Euler(timeOfDay * 360f, 180, 0);
+        Moon.transform.localRotation = Quaternion.Euler(timeOfDay * 360f + 180f, 180, 0);
+        
+        Sun.intensity = sunIntensity * SunCurve.Evaluate(timeOfDay);
+        Moon.intensity = moonIntensity * MoonCurve.Evaluate(timeOfDay);
+    }
+}
