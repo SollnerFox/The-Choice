45ab936f1d799fe1aad67bbbabcd01dcb1db6d9b
SollnerFox <milan.crusnik.02@gmail.com>
2022-06-14 22:08:10 +0300

    Island
    Add island and some grass

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,19 +5,46 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _speed = 12f;
-    private CharacterController controller;
-    void Start()
+    [SerializeField] private float _gravity = -9.81f;
+    [SerializeField] private float _jumpHeight = 3f;
+    [SerializeField] private Transform _groundCheck;
+    [SerializeField] private float _groundDistance = 0.4f;
+    [SerializeField] private LayerMask _groundMask;
+    private Vector3 velocity;
+    private bool isGrounded;
+    private CharacterController characterController;
+    private void Start()
     {
-        controller = GetComponent<CharacterController>();
+        characterController = GetComponent<CharacterController>();
     }
 
 
-    void Update()
+    private void Update()
     {
+        isGrounded = Physics.CheckSphere(_groundCheck.position, _groundDistance, _groundMask);
+        
+
+        if (isGrounded && velocity.y < 0) 
+        {
+            velocity.y = -2f;
+        } 
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 moveDirection = transform.right * x +    transform.forward * z;
-        controller.Move(moveDirection * _speed * Time.deltaTime);
+        velocity.y += _gravity * Time.deltaTime;
+        characterController.Move(moveDirection * _speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(_jumpHeight * -2f * _gravity); 
+        }
+
+        characterController.Move(velocity * Time.deltaTime);
+    }
+    private void OnDrawGizmos() 
+    {
+        Gizmos.DrawSphere(_groundCheck.position, _groundDistance);
     }
 }
